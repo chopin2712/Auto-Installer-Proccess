ff47e65c1db18090733b78da84922f9334c96aad
Chopin <julien.lejoly2712@gmail.com>
2019-06-26 20:46:03 +0200

    Adding dependency section

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+# Dependencies
+sudo apt install python3.7
+sudo apt install wine
+sudo apt install git
+sudo apt install snap
+
+# Install
 sudo mkdir /opt/aip
 sudo cp * /opt/aip
 sudo cp /opt/aip/aip /usr/bin
